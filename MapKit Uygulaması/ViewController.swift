8d49abf6f0614a5dc910415794fad039a74c25bc
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-04 23:47:31 +0300

    Ders 2-MapKit ve CLLocation Giriş

--- MapKit Uygulaması/ViewController.swift
+++ MapKit Uygulaması/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager  = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    func konumServisKontrol() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            // konum servisi açılmıştır
+            ayarlaLocationManager()
+            izinKontrol()
+        } else {
+            
+        }
     }
 
-
+    func ayarlaLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func izinKontrol() {
+        switch CLLocationManager.authorizationStatus() {
+            
+        case .authorizedAlways :
+            //Kullanıcı daima kullanmamız için izin vermiştir
+            break
+        case .authorizedWhenInUse :
+            //uygulama sadece kullanım esnasında konum bilgisine erişebilir
+            break
+        case .denied :
+            //kullanıcı izin isteğimizi reddetmiştir veya henüz izin vermemiştir
+            break
+        case .notDetermined :
+            //kullanıcı henüz karar vermemiştir
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted :
+            //uygulamamızın durumu kısıtlanmıştır.
+            break
+            
+        }
+        
+    }
 }
 
+extension ViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Konum güncellenirse tetiklenir
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //Konuma verilen izin değiştirilirse
+    }
+    
+}
